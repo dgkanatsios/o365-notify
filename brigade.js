01f99bfbc39c7aa2bfa130b756545cef3a756fb3
Dimitris-Ilias Gkanatsios <dgkanatsios@outlook.com>
2018-12-05 14:16:26 +0200

    added echo

--- brigade.js
+++ brigade.js
@@ -16,5 +16,15 @@ events.on("push", (e, p) => {
 
 
 events.on("webhook", (e, p) => {
-  console.log(p);
+  var echo = new Job("echo", "alpine:3.4");
+  echo.tasks = [
+    "echo Project " + p.name,
+    "echo Event $EVENT_NAME"
+  ];
+
+  echo.env = {
+    "EVENT_NAME": e.type
+  };
+
+  echo.run();
 });
